5883f5ead62e82ce233970e4a237c63021e11789
shukubota <kubotashu0421@gmail.com>
2025-01-26 21:16:58 +0900

    試しに画面

---
+++ app/(tabs)/top.tsx
@@ -0,0 +1,18 @@
+import {StyleSheet, ScrollView, View, Text, SafeAreaView} from 'react-native';
+
+export default function TabTopScreen() {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>
+            TOP
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+});
